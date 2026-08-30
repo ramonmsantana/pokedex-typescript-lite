8182668a7ffe2dd35875004f165067dacf451c96
ramonmsantana <ramonsantana89@hotmail.com>
2026-08-30 00:59:23 -0300

    feat> cria interfaces de pokemon

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export interface PokemonResumo {
+    id: number;
+    nome: string;
+    tipos: string[];
+    altura: number;
+    peso: number;
+}
+
+export interface PokemonApiResponse {
+    id: number;
+    name: string;
+    height: number;
+    weight: number;
+    types: {
+        name: string
+    }[],
+}
